1ad046a8e615b9de36db9fc825147e5594c83231
TobyMayn <tobiasmayn@gmail.com>
2026-01-06 20:41:34 +0100

    create basic app, adding items

--- mobile/smart-shopper/app/index.tsx
+++ mobile/smart-shopper/app/index.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppHeader from '../components/AppHeader';
-import {View, ScrollView, Text} from 'react-native';
+import {View, ScrollView, Text, TextInput, StyleSheet, Button} from 'react-native';
 
 const Index = () => {
 
+  const [query, setQuery] = useState('');
+  const [cardEmpty, setCardEmpty] = useState(true);
+  const [cart, setCart] = useState<string[]>([]);
+  function addItem(item: string){
+    if (cardEmpty){
+      setCardEmpty(false);
+    }
+    setCart(cart.concat(item.trim()));
+  }
   return (
     <View>
-      <AppHeader />
+      <View>
+        <AppHeader />
+      </View>
 
       <ScrollView>
-        <Text>Hello world! </Text>
-      </ScrollView>
+        <Text>Add grocery:</Text>
+        <TextInput 
+          style={inputStyle.inputField}
+          placeholder='Add Grocery'
+          value={query}
+          onChangeText={(e) => setQuery(e)}
+        />
+        <Button title='Add' onPress={() =>{addItem(query)}}/>Add
 
+      {cardEmpty && cart.length === 0 && (
+        <View>
+          <Text>Card is empty</Text>
+          <Text>Add items to the card</Text>
+        </View>
+      )}
+      {cart.length > 0 && (
+        <View>
+          {cart.map((item, index) => (
+          <Text key={index}>{index + ' ' + item}</Text>
+        ))}
+        </View>
+      )}
+      </ScrollView>
     </View>
   );
 };
 
+const inputStyle = StyleSheet.create({
+  inputField: {
+    borderWidth: 1,
+    color: 'black',
+  }
+})
 export default Index;
