84b9f9fa30793b08a432ab87a6ae8d8f5d4c2115
TobyMayn <tobiasmayn@gmail.com>
2026-01-13 18:01:13 +0100

    Add lucide trash icon for in remove button

--- mobile/smart-shopper/components/CartItem.tsx
+++ mobile/smart-shopper/components/CartItem.tsx
@@ -1,4 +1,5 @@
 import { Text, View, Pressable } from "react-native";
+import { Trash2 } from "lucide-react-native";
 
 interface CartItemProps {
     id: number;
@@ -11,8 +12,8 @@ export function CartItem({ id, onRemove, item }: CartItemProps){
     return (
         <View className="flex flex-row m-2 p-2">
             <Text key={id} className="flex-1 align-middle">{item}</Text>
-            <Pressable onPress={()=>{onRemove(id)}} className="border-1 rounded-md p-1">
-                <Text>Remove Item</Text>
+            <Pressable onPress={()=>{onRemove(id)}} className="bg-red-200 rounded-md p-2">
+                <Trash2 color="#fb2c36"/>
             </Pressable>
         </View>
         
