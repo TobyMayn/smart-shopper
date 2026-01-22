583a26e7b9208a97bd936c7fa3a87cc9af04844c
TobyMayn <tobiasmayn@gmail.com>
2026-01-22 13:28:00 +0100

    calculate and add total and saving prices to grocery cart view

--- mobile/smart-shopper/components/GroceryCart.tsx
+++ mobile/smart-shopper/components/GroceryCart.tsx
@@ -1,17 +1,30 @@
 import {View, Pressable, Text} from 'react-native';
 import { CartItem } from './CartItem';
+import { ShoppingListItem } from '@/types/grocery';
 
 interface GroceryCartProps {
-    cart: string[];
+    cart: ShoppingListItem[];
     onRemove: (id: number) => void;
     cartEmpty: boolean;
 
 }
 
 export function GroceryCart({cart, onRemove, cartEmpty}: GroceryCartProps) {
+    // Calculate totals
+  const totalCheapest = cart.reduce((sum, item) => {
+    return sum + (item.cheapestPrice?.price || 0) * item.quantity;
+  }, 0);
+
+  const totalSavings = cart.reduce((sum, item) => {
+    return sum + (item.potentialSavings || 0) * item.quantity;
+  }, 0);
 
     return (
         <View>
+            <View className='flex flex-row m-2 p-2'>
+                <Text className='flex-1'>Total: {totalCheapest.toFixed(2)}$</Text>
+                <Text className='flex-1'>Savings: {totalSavings.toFixed(2)}$</Text>
+            </View>
             {cartEmpty && cart.length === 0 && (
                 <View className='flex items-center'>
                     <Text className='italic'>Card is empty</Text>
