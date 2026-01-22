c0c49be7541091583841d52504c2cfc1519e88d6
TobyMayn <tobiasmayn@gmail.com>
2026-01-22 13:26:59 +0100

    add cheapest found price to item cards

--- mobile/smart-shopper/components/CartItem.tsx
+++ mobile/smart-shopper/components/CartItem.tsx
@@ -1,17 +1,22 @@
 import { Text, View, Pressable } from "react-native";
 import { Trash2 } from "lucide-react-native";
+import { ShoppingListItem } from "@/types/grocery";
 
 interface CartItemProps {
     id: number;
     onRemove: (id: number) => void;
-    item: string;
+    item: ShoppingListItem;
 }
 
 export function CartItem({ id, onRemove, item }: CartItemProps){
-
+    const cheapest = item.cheapestPrice;
+    const savings = item.potentialSavings;
     return (
         <View className="flex flex-row m-2 p-2">
-            <Text key={id} className="flex-1 align-middle">{item}</Text>
+            <Text key={id} className="flex-1 align-middle">{item.name}</Text>
+            {cheapest && (
+                <Text className="flex-1 align-middle">{cheapest.price.toFixed(2)}$</Text>
+            )}
             <Pressable onPress={()=>{onRemove(id)}} className="bg-red-200 rounded-md p-2">
                 <Trash2 color="#fb2c36"/>
             </Pressable>
