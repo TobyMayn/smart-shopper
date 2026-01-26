c145f6b6b72ca92853f22634430946165d53210d
TobyMayn <tobiasmayn@gmail.com>
2026-01-26 21:08:41 +0100

    create file for scraping grocery store pages. As no danish stores has a usable api

---
+++ scripts/grocery_scraper.py
@@ -0,0 +1,7 @@
+def GroceryScraper():
+    '''
+    GroceryScraper: A web-scraper to scrape for groceries.
+    
+    At first it will only be able to scrape major danish grocery stores.
+    '''
+    return
