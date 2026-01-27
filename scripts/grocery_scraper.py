42f746ab29d67ad0d665c70bc4e3a0bb735c4903
TobyMayn <tobiasmayn@gmail.com>
2026-01-27 13:11:22 +0100

    setup basic scraper for rema 1000 online store

--- scripts/grocery_scraper.py
+++ scripts/grocery_scraper.py
@@ -1,7 +1,34 @@
-def GroceryScraper():
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class GroceryScraper():
     '''
     GroceryScraper: A web-scraper to scrape for groceries.
     
     At first it will only be able to scrape major danish grocery stores.
     '''
-    return
+    def main(self):
+
+        options = Options()
+        options.add_argument("--headless") # Runs without opening a window
+        driver = webdriver.Chrome(options=options)
+
+        driver.get("https://shop.rema1000.dk/avisvarer/")
+        time.sleep(5) # Give the JavaScript a few seconds to load the products
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        print(soup)
+        # Now you can pass 'html' into BeautifulSoup
+        driver.quit()
+         
+        
+        
+
+
+
+GroceryScraper().main()
